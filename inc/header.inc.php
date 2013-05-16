832f4a014beac91b4d9f0caf370ab147d1c8ca80
Alexjan Carraturo <a.carraturo@mrd-institute.com>
2013-05-16 13:51:40 +0200

    Initial Commit: test php and git server

---
+++ inc/header.inc.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Created on 10/mag/2013
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+
+
+<?php
+
+echo "<html>" .
+		"<header>" .
+		"<title> Time Manager</title>" .
+		"</header>";
+
+echo "<body>";
+
+
+
+?>
---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Created on 10/mag/2013
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+
+include ('inc/header.inc.php');
+
+echo " <p align=center> Testo di prova </p> ";
+
+
+?>
